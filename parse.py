55075048f8acf0ab6963e74a7a1480bb03bae94f
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-08-17 08:11:26 +1000

    .

--- parse.py
+++ parse.py
@@ -325,6 +325,11 @@ class SymbolNode:
                                          or (self.children[0].symbol.id == '+' and self.children[0].children[1].token.category == Token.Category.STRING_LITERAL)):
                 c1 = self.children[1].to_str()
                 return self.children[0].to_str() + ('(' + c1 + ')' if c1[0] == '.' else c1)
+            elif self.symbol.id == '+' and self.children[1].symbol.id == '*' and self.children[1].children[0].token.category == Token.Category.STRING_LITERAL: # for `self.newlines() + ' ' * (indent*3) + 'F ' + ...`
+                return self.children[0].to_str() + '‘’(' + self.children[1].to_str() + ')'
+            elif self.symbol.id == '+' and self.children[0].symbol.id == '*' and self.children[0].children[0].token.category == Token.Category.STRING_LITERAL: # for `' ' * (indent*3) + self.expression.to_str() + "\n"`
+                c1 = self.children[1].to_str()
+                return '(' + self.children[0].to_str() + ')‘’' + ('(' + c1 + ')' if c1[0] == '.' else c1)
             elif self.symbol.id == '+' and (self.children[0].var_type() == 'str' or self.children[1].var_type() == 'str'):
                 return self.children[0].to_str() + '‘’' + self.children[1].to_str()
             elif self.symbol.id == '<=' and self.children[0].symbol.id == '<=': # replace `if '0' <= ch <= '9'` with `I ch C ‘0’..‘9’`

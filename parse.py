6e772c6870d1089323f77860337fc05e29540a09
Alexander Tretyak <alextretyak@users.noreply.github.com>
2021-12-02 11:32:40 +1000

    `product_of_a_seq` -> `product` (for https://www.rosettacode.org/wiki/Numbers_divisible_by_their_individual_digits,_but_not_by_the_product_of_their_digits.)

--- parse.py
+++ parse.py
@@ -93,7 +93,7 @@ class Scope:
     def find_and_get_prefix(self, name, token):
         if name == 'self':
             return ''
-        if name in ('isinstance', 'len', 'super', 'print', 'input', 'ord', 'chr', 'int_to_str_with_radix', 'range', 'zip', 'all', 'any', 'abs', 'pow', 'sum', 'product',
+        if name in ('isinstance', 'len', 'super', 'print', 'input', 'ord', 'chr', 'int_to_str_with_radix', 'range', 'zip', 'all', 'any', 'abs', 'pow', 'sum', 'product_of_a_seq', 'product',
                     'open', 'min', 'max', 'divmod', 'hex', 'hexu', 'rotl32', 'bin', 'map', 'list', 'tuple', 'dict', 'set', 'sorted', 'reversed', 'filter', 'reduce', 'cmp_to_key',
                     'next_permutation', 'is_sorted', 'format_float', 'format_float_exp', 'move', 'ref',
                     'round', 'enumerate', 'hash', 'copy', 'deepcopy', 'NotImplementedError', 'ValueError', 'IndexError', 'RuntimeError'):
@@ -734,6 +734,8 @@ class SymbolNode:
                         if i < len(self.children)-2:
                             res += ', '
                     return res + ')'
+                elif func_name == 'product_of_a_seq':
+                    func_name = 'product'
                 elif func_name == 'product':
                     func_name = 'cart_product'
                 elif func_name == 'deepcopy':

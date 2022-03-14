bc980a12049292c6990258dfc7a8c8a9dabf526c
Alexander Tretyak <alextretyak@users.noreply.github.com>
2022-03-14 21:02:40 +1000

    Functions `math.ceil()` and `math.floor()` return `int` in Python [for https://codeforces.com/contest/1521/submission/117139834]

--- parse.py
+++ parse.py
@@ -1195,6 +1195,7 @@ class SymbolNode:
                         c1 = self.children[1].to_str()
                         if c1 not in ('e', 'pi'):
                             if c1 == 'fabs': c1 = 'abs'
+                            elif c1 in ('ceil', 'floor'): c1 += 'i' # functions `math.ceil()` and `math.floor()` return `int` in Python
                             return c1
                     r = self.children[0].token_str() + ':' + self.children[1].to_str()
                     return {'tempfile:gettempdir': 'fs:get_temp_dir', 'os:path': 'fs:path', 'os:pathsep': 'os:env_path_sep', 'os:sep': 'fs:path:sep', 'os:system': 'os:', 'os:listdir': 'fs:list_dir', 'os:walk': 'fs:walk_dir',
